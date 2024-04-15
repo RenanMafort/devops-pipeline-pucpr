3e3e6c5bb820e41ddff9b50bef3e1c4b67215fe7
RenanMafort <94027729+RenanMafort@users.noreply.github.com>
2024-04-15 20:52:58 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,8 @@
+lista_of_numbers = list(range(1,51))
+numbers = 0;
+for i in lista_of_numbers:
+  if i % 2 == 0:
+    numbers += i
+
+print(numbers)
+print("################## FIM ######################")
